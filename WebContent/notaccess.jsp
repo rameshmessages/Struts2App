f9dd99695d81144914bbfb5fb0363d7f120a82be
rameshmessages <rameshmessages@gmail.com>
2017-05-31 16:12:35 +0530

    Struts2App
    Struts2 Example Project

---
+++ WebContent/notaccess.jsp
@@ -0,0 +1,6 @@
+<%@ taglib uri="/struts-tags" prefix="s" %>
+<h1> Struts2 First Application- You are not authorized</h1>
+You are not allowed to enter the below values<br/>
+Product Id:<s:property value="id"/><br/>
+Product Name:<s:property value="name"/><br/>
+Product Price:<s:property value="price"/><br/>
---
+++ src/com/lstech/struts2/Product.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package com.lstech.struts2;
+
+/**
+ * @author ramesh
+ *
+ */
+public class Product {
+	
+	private int id;
+	private String name;
+	private float price;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public String execute(){
+		
+		if (price <= 100000)
+		{
+			return "success";	
+		}else{
+			return "failure";
+		}
+	
+	}
+
+}
